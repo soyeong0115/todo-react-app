c62b349e133c6c5ca29c38dd7fd62253a4d8f57b
soyeong <mb535622@sookmyung.ac.kr>
2023-04-17 01:10:54 +0900

    프론트 개발 1~6. React 로 안녕하세요! 출력

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+class App extends React.Component { 
+  render(){ 
+    return (
+ <div className="App">
+ <Hello />
+ </div>
+ ); 
+}
+}
+
+export default App;
+
+
